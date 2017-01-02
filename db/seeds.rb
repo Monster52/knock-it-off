5f9127ec33bc338ad6266537c9e1c0c5a29e1f2e
Ross Waguespack <rosswaguespack@gmail.com>
2017-01-01 20:59:48 -0600

    Seeded the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create( email: 'ross@email.com', password: 'password' )
+puts "Created admin email #{admin.email} : password #{admin.password}"
+
+5.times do
+  User.create(
+       email: Faker::Internet.email,
+       password: 'password'
+    )
+end
+
+users = User.all
+puts "Created #{users.count} users"
+
+50.times do
+  Item.create(
+      name: Faker::Hacker.verb,
+      user: users.sample 
+    )
+end
+
+items = Item.all
+puts "Created #{items.count} items"
